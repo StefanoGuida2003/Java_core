a63f9559fb73da0f35072f9614bbd842140b39f5
Stefano Guida <stefan0guid4@gmail.com>
2023-06-22 17:23:43 +0200

    Hello World is now even/odd

--- hello_world/HelloWorld.java
+++ hello_world/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld{
 	
 	public static void main(String[] args){
-		System.out.println("Hello World! I'm " + args[0]);
+		int n = Integer.parseInt(args[0]);
+		if(n % 2 == 0) {
+			System.out.println(args[0] + " is even");	 
+		}
+		else {
+			System.out.println(args[0] + " is odd");
+		}
 	}
 }
